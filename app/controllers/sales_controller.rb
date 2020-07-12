ca43a05013d425b968a803e88372557430905ef1
Sara Lonngren <saralonngren@gmail.com>
2020-07-11 23:01:16 -0300

    Created import file route and improved index

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,23 +1,41 @@
+require 'csv'
+
 class SalesController < ApplicationController
 
   def index
     @sales = Sale.all
+    @total_price = params[:total_price]
   end
 
-  def new
-    @sale = Sale.new
-  end
+  def import_file
+    raise 'Arquivo não encontrado' unless params[:file].present?
 
-  def create
-    # binding.pry
-    # @file = params[:file].path
-
-    # csv = CSV.new(@file, headers: true, col_sep: '\t')
+    total_price = create_sale_and_count_prince(params[:file])
+    redirect_to sales_path(total_price: total_price), notice: 'Sales uploaded successfully'
+  
+  rescue StandardError => e
+    redirect_to sales_path, notice: "#{e.message} Please verify the file and try again."
   end
 
   private
 
-  # def content_type_valid?
-  #   %w(text/csv application/octet-stream application/vnd.ms-excel).include?(@content_type)
-  # end
+  def create_sale_and_count_prince(file)
+    total_price = 0
+
+    CSV.foreach(file, headers: true, col_sep: "\t") do |row|
+      sale = row.to_h
+      total_price += (sale['item price'].to_f * sale['purchase count'].to_i ) 
+
+      Sale.create(
+        purchaser_name: sale['purchaser name'],
+        item_description: sale['item description'],
+        item_price: sale['item price'].to_f,
+        purchase_count: sale['purchase count'].to_i,
+        merchant_address: sale['merchant address'],
+        merchant_name: sale['merchant name']
+      )
+    end
+
+    total_price
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'sales#index'
 
-  resources :sales
+  resources :sales, only: [:index]
+  post 'sales/import_file'
 end
